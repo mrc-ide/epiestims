95225ddbf2368762111ab43abf8823ed99d7c37a
Anne Cori <a.cori@imperial.ac.uk>
2021-04-21 17:04:19 +0100

    more exploration of gibbs sampler for epsilon - seems to return what I would expect

--- MultiLocEpiEstim/R/gibbs_draws.R
+++ MultiLocEpiEstim/R/gibbs_draws.R
@@ -14,9 +14,9 @@ compute_lambda <- function(incid, w) {
 }
 
 draw_epsilon <- function(R, incid, lambda, priors) {
-  shape <- priors$epsilon$shape + sum(incid[, , 1])
-  rate <- 1 / priors$epsilon$scale +
-    sum(R * lambda[, , 1, drop = FALSE], na.rm = TRUE)
+  # ignoring the first time step as we are conditioning on this
+  shape <- sum(incid[-1, , 1]) + priors$epsilon$shape
+  rate <- sum(R[-1, , ] * lambda[-1, , 1]) + 1 / priors$epsilon$scale
   scale <- 1 / rate
   rgamma(1, shape = shape, scale = scale)
 }
@@ -24,17 +24,27 @@ draw_epsilon <- function(R, incid, lambda, priors) {
 
 
 
-n_v <- 2
-n_loc <- 3
-T <- 20
+n_v <- 2 # 2 variants
+n_loc <- 3 # 3 locations
+T <- 100 # 20 time steps
+
 priors <- default_priors()
-incid <- array(rpois(n_v * n_loc * T, 10), dim = c(T, n_loc, n_v))
+
+# constant incidence 10 per day everywhere
+incid <- array(10, dim = c(T, n_loc, n_v))
+
+# arbitrary serial interval
 w_v <- c(0, 0.2, 0.5, 0.3)
 for(i in seq(2, n_v)) {
   w <- cbind(w, w_v)
 }
 lambda <- compute_lambda(incid, w)
-R <- array(runif((n_v - 1) * n_loc * T, 0.5, 2.5), dim = c(T, n_loc, n_v - 1))
+
+# Reproduction number of 1
+R <- array(1, dim = c(T, n_loc, n_v - 1))
 R[1, , ] <- NA # no estimates of R on first time step
 
 draw_epsilon(R, incid, lambda, priors)
+x <- sapply(1:1000, function(e) draw_epsilon(R, incid, lambda, priors))
+mean(x)
+median(x) ### This should be 1 - it is not 1 because of the first few timesteps AND because of priors - but close enough
