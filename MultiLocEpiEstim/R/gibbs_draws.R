aeaa6afb4841060cf7e0b239e2f9e0dea4668f9e
Anne Cori <a.cori@imperial.ac.uk>
2021-04-21 20:30:05 +0100

    moving random bits of code into proper test

--- MultiLocEpiEstim/R/gibbs_draws.R
+++ MultiLocEpiEstim/R/gibbs_draws.R
@@ -13,38 +13,20 @@ compute_lambda <- function(incid, w) {
   lambda
 }
 
-draw_epsilon <- function(R, incid, lambda, priors) {
-  # ignoring the first time step as we are conditioning on this
-  shape <- sum(incid[-1, , 1]) + priors$epsilon$shape
-  rate <- sum(R[-1, , ] * lambda[-1, , 1]) + 1 / priors$epsilon$scale
+draw_epsilon <- function(R, incid, lambda, priors,
+                         t_min = 2, t_max = nrow(incid),
+                         seed = NULL) {
+  ## TODO: check t_min and t_max are integers, >=2 and <= nrow(incid)
+  ## TODO: check seed is a numeric value
+  if (!is.null(seed)) set.seed(seed)
+  t <- seq(t_min, t_max, 1)
+  shape <- sum(incid[t, , 1]) + priors$epsilon$shape
+  rate <- sum(R[t, , ] * lambda[t, , 1]) + 1 / priors$epsilon$scale
   scale <- 1 / rate
   rgamma(1, shape = shape, scale = scale)
 }
 
+## TODO: somewhere need to add:
+## dimension checks for incid, R, w
 
 
-
-n_v <- 2 # 2 variants
-n_loc <- 3 # 3 locations
-T <- 100 # 20 time steps
-
-priors <- default_priors()
-
-# constant incidence 10 per day everywhere
-incid <- array(10, dim = c(T, n_loc, n_v))
-
-# arbitrary serial interval
-w_v <- c(0, 0.2, 0.5, 0.3)
-for(i in seq(2, n_v)) {
-  w <- cbind(w, w_v)
-}
-lambda <- compute_lambda(incid, w)
-
-# Reproduction number of 1
-R <- array(1, dim = c(T, n_loc, n_v - 1))
-R[1, , ] <- NA # no estimates of R on first time step
-
-draw_epsilon(R, incid, lambda, priors)
-x <- sapply(1:1000, function(e) draw_epsilon(R, incid, lambda, priors))
-mean(x)
-median(x) ### This should be 1 - it is not 1 because of the first few timesteps AND because of priors - but close enough
