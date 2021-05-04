7fc7de94d2fdf298061a74379d671fa2b9443ad2
sangeetabhatia03 <sangeetabhatia03@gmail.com>
2021-05-04 14:17:22 +0100

    Set up for varying tmax

--- simulations/simulation_functions.R
+++ simulations/simulation_functions.R
@@ -1,10 +1,17 @@
-process_fit <- function(fit, probs = c(0.025, 0.25, 0.5, 0.75, 0.975)) {
+process_fit <- function(fit, probs = c(0.025, 0.25, 0.5, 0.75, 0.975), na.rm = TRUE) {
 
   ## Get rid of the first row because of NAs
   r_est <- apply(
     fit$R[-1, , ], c(1, 2), quantile,
-    probs = probs, na.rm = TRUE
+    probs = probs, na.rm = na.rm
   )
+  r_mu <- apply(
+    fit$R[-1, , ], c(1, 2), mean, na.rm = na.rm
+  )
+  r_sd <- apply(
+    fit$R[-1, , ], c(1, 2), sd, na.rm = na.rm
+  )
+
   nt <- dim(r_est)[2]
   nl <- dim(r_est)[3]
   r_estdf <- data.frame(
@@ -15,6 +22,8 @@ process_fit <- function(fit, probs = c(0.025, 0.25, 0.5, 0.75, 0.975)) {
     `50%` = rep(NA, nt * nl),
     `75%` = rep(NA, nt * nl),
     `97.5%` = rep(NA, nt * nl),
+    mu = rep(NA, nt * nl),
+    sd = rep(NA, nt * nl),
     check.names = FALSE
   )
   r_estdf$time <- rep(seq_len(nt), each = nl)
@@ -22,17 +31,21 @@ process_fit <- function(fit, probs = c(0.025, 0.25, 0.5, 0.75, 0.975)) {
   for (time in seq_len(nt)) {
     for (location in seq_len(nl)) {
       r_estdf[r_estdf$time == time & r_estdf$location == location, 3:7] <- r_est[, time, location]
+      r_estdf[r_estdf$time == time & r_estdf$location == location, "mu"] <- r_mu[time, location]
+      r_estdf[r_estdf$time == time & r_estdf$location == location, "sd"] <- r_sd[time, location]
     }
   }
   r_estdf$param <- "R"
   epsilon_est <- quantile(
-    fit$epsilon, probs = probs, na.rm = TRUE
+    fit$epsilon, probs = probs, na.rm = na.rm
   )
   eps_df <- data.frame(time = NA, location = NA)
   eps_df <- cbind(eps_df, epsilon_est)
   ## Stupid tall. make wide
   eps_df <- tibble::rownames_to_column(eps_df)
   eps_df <- tidyr::spread(eps_df, rowname, epsilon_est)
+  eps_df$mu <- mean(fit$epsilon, na.rm  = na.rm)
+  eps_df$sd <- sd(fit$epsilon, na.rm  = na.rm)
   eps_df$param <- "epsilon"
   rbind(eps_df, r_estdf)
 }
--- simulations/simulations.R
+++ simulations/simulations.R
@@ -12,159 +12,181 @@ library(incidence)
 library(purrr)
 library(dplyr)
 library(ggplot2)
-
+library(here)
 source("simulations/simulation_functions.R")
 
+if (! dir.exists("figures")) dir.create("figures")
 seed <- 42
 set.seed(seed)
-
+## Common parameters
 ## Set time, locations and number of variants in simulations
 ndays <- 200
 n_loc <- 3
 n_v <- 2
 
-## Set-up reference reproduction numbers in each location
-rt_ref <- c(1.5, 1.2, 1.1)
+## Reference reproduction numbers in each location
+rt_ref <- c(1.1, 1.1, 1.1)
 
-## Define range of transmission advantage values to explore
+## Range of transmission advantage values to explore
 transmission_advantage <- seq(1, 2, 0.2)
+names(transmission_advantage) <- transmission_advantage
 
 ## Define range of tmax values to explore
 tmax_all <- seq(200, 40, -20)
+tmax_all <- as.integer(tmax_all)
+names(tmax_all) <- tmax_all
 
 ## Set serial interval distributions
 ## TO DO: vary these (eg variant has si that is shorter or longer than ref)
 ## covid serial interval from IBM
 si_mean <- 6.83
 si_std <- 3.8
-si <- EpiEstim::discr_si(0:30, mu = si_mean, sigma = si_std)
+si <- discr_si(0:30, mu = si_mean, sigma = si_std)
 si <- si / sum(si)
 si_no_zero <- si[-1]
+## Needs to change if number of variants is
+## changed.
 si_distr <- cbind(si, si)
+priors <- EpiEstim:::default_priors()
+mcmc_controls <- list(
+  n_iter = 1e4L, burnin = as.integer(floor(1e4 / 2)),
+  thin = 10L
+)
+
+
+simulated_incid <- map(
+  transmission_advantage, function(advantage) {
+    message("transmission advantage = ", advantage)
+    ## Calculate reproduction number for variant
+    rt_variant <- advantage * rt_ref
+    ## Assume reproduction number remains the same
+    ## over the time period
+    ## Make a vector that goes across rows
+    R <- array(NA, dim = c(ndays, n_loc, n_v))
+    ## TO DO: update this so that it can handle n_v variants
+    R[,,1] <- rep(rt_ref, each = ndays)
+    R[,,2] <- rep(rt_variant, each = ndays)
+  ##############################################################################
+  ## Simulate epidemic incidence data with input reproduction numbers and si  ##
+  ##############################################################################
+  ## start with 20 infected individuals
+  ## large-ish initial seed to ensure we get an epidemic with lower values of R
+  ## can explore lower seed numbers later
+    initial_incidence <- incidence::incidence(rep(1, 20))
+    simulate_incidence(
+      initial_incidence, n_loc, n_v, ndays, R, si_distr
+    )
+})
 
 ## Run simulations across all combinations of transmission_advantage and tmax_all
 ## TO DO: convert code below into function that takes various variables above as arguments
 ## then we can also look at changes in si
-results <- map(transmission_advantage, function(advantage) {
-  
-  message("transmission advantage = ", advantage)
-  
-  out <- list(incid = NA,
-              vary_tmax_est = NA)
-  
-  ## Calculate reproduction number for variant
-  rt_variant <- advantage * rt_ref
-  ## Assume reproduction number remains the same
-  ## over the time period
-  ## Make a vector that goes across rows
-  R <- array(NA, dim = c(ndays, n_loc, n_v))
-  R[,,1] <- rep(rt_ref, each = ndays) 
-  R[,,2] <- rep(rt_variant, each = ndays) ## TO DO: update this so that it can handle n_v variants
-  
-  ##############################################################################
-  ## Simulate epidemic incidence data with input reproduction numbers and si  ##
-  ##############################################################################
-  
-  ## start with 20 infected individuals
-  ## large-ish initial seed to ensure we get an epidemic with lower values of R
-  ## can explore lower seed numbers later
-  initial_incidence <- incidence::incidence(rep(1, 20))
-  
-  incid <- array(NA, dim = c(ndays, n_loc, n_v))
-  
-  for (loc in 1:n_loc) {
-    for (v in 1:n_v) {
-      incid[, loc, v] <- rbind(
-        initial_incidence$counts,
-        as.matrix( #
-          projections::project(initial_incidence,
-                               R = R[-1, loc, v], # R in the future so removing time of seeding
-                               si = si_no_zero,
-                               n_sim = 1,
-                               n_days = ndays - 1,
-                               time_change = seq_len(length(R[, loc, v]) - 2) - 1
-          )
-        )
-      )
-    }
-  }
-  
-  out[["incid"]] <- incid
-  
-  ## Now estimate epsilon
-  priors <- EpiEstim:::default_priors()
-  mcmc_controls <- list(n_iter = 1e4L, burnin = as.integer(floor(1e4 / 2)), thin = 10L)
-  
-  ## x is a list with elements R and epsilon
-  ## epsilon is  a vector with length (n_iter - burnin)/thin + 1
-  ## R is an array with dimensions T X n_loc X (n_iter - burnin)/thin + 1
-  
+results <- map(
+  simulated_incid, function(incid) {
   ## How does varying amount of data affect estimates?
-  tmax_all <- as.integer(tmax_all)
-  names(tmax_all) <- tmax_all
-  vary_tmax_fits <- map(
-    tmax_all, function(tmax) {
+    map(tmax_all, function(tmax) {
       message("tmax = ", tmax)
       EpiEstim:::estimate_joint(
-        incid, si_distr, priors, seed = 1,
-        t_min = 2L, t_max = tmax,
-        mcmc_control = mcmc_controls
-      )
+       incid, si_distr, priors, seed = 1,
+       t_min = 2L, t_max = tmax,
+       mcmc_control = mcmc_controls
+       )
     }
   )
-  
-  ## Process fits
-  vary_tmax_est <- imap_dfr(
-    vary_tmax_fits, function(out, tmax) {
-      message("tmax = ", tmax)
-      process_fit(out)
+ }
+)
+
+## Maximum
+## cumulative incidence at tmax across
+## locations variants
+cum_incid <- map_dfr(
+  simulated_incid, function(incid) {
+    map_dfr(tmax_all, function(tmax) {
+      x <- incid[seq_len(tmax), ,]
+      out <- apply(x, c(2, 3), sum)
+      data.frame(
+        max_cum_incid = max(out)
+      )
     }, .id = "tmax"
-  )
-  
-  vary_tmax_est$tmax <- factor(
-    vary_tmax_est$tmax, levels = rev(tmax_all), ordered = TRUE
-  )
-  
-  out[["vary_tmax_est"]] <- vary_tmax_est
-  
-  out
-  
-})
+    )
+  }, .id = "epsilon"
+)
 
-## Plot epsilon estimates
-names(results) <- transmission_advantage
-estimates_combined <- map_dfr(results, function(advantage) {
-  
-  advantage[["vary_tmax_est"]]
-  
-}, .id = "transmission_advantage")
-  
-estimates_combined$transmission_advantage <- as.numeric(estimates_combined$transmission_advantage)
+vary_tmax_est <- map_depth(
+  results, 2, process_fit
+)
 
-trans_adv <- estimates_combined %>% 
-  group_by(transmission_advantage) %>% 
-  summarise(adv = mean(transmission_advantage))
+vary_tmax_est <- map_dfr(
+  vary_tmax_est, function(x) {
+    bind_rows(x, .id = "tmax")
+  }, .id = "epsilon"
+)
 
-ggplot(data = estimates_combined[estimates_combined$param == "epsilon", ]) +
-  geom_linerange(aes(tmax, ymin = `2.5%`, ymax = `97.5%`)) +
-  geom_point(aes(tmax, `50%`)) +
-  geom_hline(data = trans_adv, aes(
-    yintercept = adv),
-    linetype = "dashed", color = "red"
+vary_tmax_est <- left_join(vary_tmax_est, cum_incid)
+
+vary_tmax_est$tmax <- factor(
+  vary_tmax_est$tmax, levels = rev(tmax_all), ordered = TRUE
+)
+## Duplicate column so that we can plot true values
+vary_tmax_est$true_epsilon <- as.numeric(vary_tmax_est$epsilon)
+
+vary_tmax_est$epsilon <- factor(
+  vary_tmax_est$epsilon,
+  levels = transmission_advantage,
+  ordered = TRUE
+)
+
+
+est_epsilon <- vary_tmax_est[vary_tmax_est$param == "epsilon", ]
+
+
+p1 <- ggplot(est_epsilon) +
+  geom_linerange(
+    aes(tmax, ymin = mu - sd, ymax = mu + sd)
   ) +
-  facet_wrap(~ transmission_advantage, nrow = 3, ncol = 4) +
-  ylab("Epsilon")
+  geom_point(aes(tmax, mu)) +
+  geom_hline(
+    aes(yintercept = true_epsilon),
+    col = "red", linetype = "dashed"
+  ) +
+  expand_limits(y = 0) +
+  ylab("epsilon") +
+  facet_wrap(~ epsilon, ncol = 2) +
+  theme_minimal() +
+  theme(
+    axis.text.x = element_text(angle = 90)
+  )
 
+cowplot::save_plot("figures/epsilon_tmax.pdf", p1)
+
+p2 <- ggplot(est_epsilon) +
+  geom_linerange(
+    aes(log(max_cum_incid), ymin = mu - sd, ymax = mu + sd)
+  ) +
+  geom_point(aes(log(max_cum_incid), mu)) +
+  geom_hline(
+    aes(yintercept = true_epsilon),
+    col = "red", linetype = "dashed"
+  ) +
+  facet_wrap(~ epsilon, ncol = 2, scales = "free_x") +
+  expand_limits(y = 0) +
+  ylab("epsilon") +
+  theme_minimal() +
+  theme(
+    axis.text.x = element_text(angle = 90)
+  )
+
+cowplot::save_plot("figures/epsilon_cumincid.pdf", p2)
 
 ## Code to check projections with plot (needs updating with correct variable names)
 ## TO DO: generate summary grid of incidence plots for each transmission advantage explored
-plot(log(1 + incid[, 1, 1]), type= "l", xlab = "date", ylab = "log(1 + Incidence)")
-lines(log(1 + incid[, 2, 1]), col = "blue")
-lines(log(1 + incid[, 3, 1]), col = "red")
-lines(log(1 + incid[, 1, 2]), lty = 2)
-lines(log(1 + incid[, 2, 2]), col = "blue", lty = 2)
-lines(log(1 + incid[, 3, 2]), col = "red", lty = 2)
-legend("bottomright", c("Strain 1, location 1", "Strain 1, location 2", "Strain 1, location 3",
-                        "Strain 2, location 1", "Strain 2, location 2", "Strain 2, location 3"),
-       col = c("black", "blue", "red", "black", "blue", "red"),
-       lty = c(1, 1, 1, 2, 2, 2), cex = 0.7)
+## plot(log(1 + incid[, 1, 1]), type= "l", xlab = "date", ylab = "log(1 + Incidence)")
+## lines(log(1 + incid[, 2, 1]), col = "blue")
+## lines(log(1 + incid[, 3, 1]), col = "red")
+## lines(log(1 + incid[, 1, 2]), lty = 2)
+## lines(log(1 + incid[, 2, 2]), col = "blue", lty = 2)
+## lines(log(1 + incid[, 3, 2]), col = "red", lty = 2)
+## legend("bottomright", c("Strain 1, location 1", "Strain 1, location 2", "Strain 1, location 3",
+##                         "Strain 2, location 1", "Strain 2, location 2", "Strain 2, location 3"),
+##        col = c("black", "blue", "red", "black", "blue", "red"),
+##        lty = c(1, 1, 1, 2, 2, 2), cex = 0.7)
