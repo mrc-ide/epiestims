c6dc9a868cb75201d3c1e91d644a66ad62875850
sangeetabhatia03 <sangeetabhatia03@gmail.com>
2021-07-13 14:00:42 +0100

    Cluster run using orderly bundle

--- run_on_cluster.R
+++ run_on_cluster.R
@@ -11,3 +11,4 @@ obj$install_packages('abind')
 obj$install_packages('mrc-ide/EpiEstim@multiv')
 res <- obj$enqueue(orderly::orderly_bundle_run('20210712-223938-7707dbc9.zip', "cluster-runs"))
 # id "4605da89c0e0ea7451c7461bea32c24c"
+orderly::orderly_bundle_import('cluster-runs/20210712-223938-7707dbc9.zip')
