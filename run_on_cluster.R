9d3d749fb3597ab120229bfd14e29f5ba9a6c3d0
sangeetabhatia03 <sangeetabhatia03@gmail.com>
2021-07-13 16:04:56 +0100

    The one that was messed up last time

--- run_on_cluster.R
+++ run_on_cluster.R
@@ -6,9 +6,11 @@ ctx <-context_save(root, packages = packages)
 # [ open:db   ]  rds
 # [ save:id   ]  ed59bb10737dc761b869e66663baef1d
 # [ save:name ]  unadored_lemur
+ctx <- context_read('ed59bb10737dc761b869e66663baef1d', root)
+# orderly::orderly_bundle_pack(".", "one_location_vary_si", parameters = list(short_run = FALSE))
 obj <- didehpc::queue_didehpc(ctx)
 obj$install_packages('abind')
 obj$install_packages('mrc-ide/EpiEstim@multiv')
-res <- obj$enqueue(orderly::orderly_bundle_run('20210712-223938-7707dbc9.zip', "cluster-runs"))
-# id "4605da89c0e0ea7451c7461bea32c24c"
+res <- obj$enqueue(orderly::orderly_bundle_run('20210713-160158-cfa36a12.zip', "cluster-runs"))
+# id "3dc0bba4842f281240837c753b7db2bb"
 orderly::orderly_bundle_import('cluster-runs/20210712-223938-7707dbc9.zip')
