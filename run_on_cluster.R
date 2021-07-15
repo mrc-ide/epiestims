c418b6cb22278b12debba2472bf0aeaf78382900
sangeetabhatia03 <sangeetabhatia03@gmail.com>
2021-07-15 14:18:23 +0100

    Mis-specified SI run

--- run_on_cluster.R
+++ run_on_cluster.R
@@ -1,4 +1,4 @@
-didehpc::didehpc_config(cores = 8)
+config <- didehpc::didehpc_config(cores = 8, parallel = TRUE)
 library(context)
 packages <- c("dplyr", "epitrix", "glue", "incidence",
               "orderly", "projections", "purrr")
@@ -9,8 +9,8 @@ ctx <-context_save(root, packages = packages)
 # [ save:name ]  unadored_lemur
 ctx <- context_read('ed59bb10737dc761b869e66663baef1d', root)
 # orderly::orderly_bundle_pack(".", "one_location_vary_si", parameters = list(short_run = FALSE))
-obj <- didehpc::queue_didehpc(ctx)
+obj <- didehpc::queue_didehpc(ctx, config = config)
 obj$install_packages('abind')
 obj$install_packages('mrc-ide/EpiEstim@multiv')
-res <- obj$enqueue(orderly::orderly_bundle_run('20210713-160158-cfa36a12.zip', "cluster-runs"))
-
+res <- obj$enqueue(orderly::orderly_bundle_run('20210715-141603-f056f3e4.zip', "cluster-runs"))
+# task id: 7d851b4088f1abe608d50ebd78d865ca
