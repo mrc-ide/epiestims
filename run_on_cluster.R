58e5918b6421b0f8cb5b9cdcddbedad3ffe4cf8d
sangeetabhatia03 <sangeetabhatia03@gmail.com>
2021-07-16 12:14:24 +0100

    Epiestim branch without reordering and vary_si task

--- run_on_cluster.R
+++ run_on_cluster.R
@@ -11,6 +11,6 @@ ctx <- context_read('ed59bb10737dc761b869e66663baef1d', root)
 # orderly::orderly_bundle_pack(".", "one_location_vary_si", parameters = list(short_run = FALSE))
 obj <- didehpc::queue_didehpc(ctx, config = config)
 obj$install_packages('abind')
-obj$install_packages('mrc-ide/EpiEstim@multiv')
-res <- obj$enqueue(orderly::orderly_bundle_run('20210715-141603-f056f3e4.zip', "cluster-runs"))
+obj$install_packages('mrc-ide/EpiEstim@draw_eps_debug')
+res <- obj$enqueue(orderly::orderly_bundle_run('20210716-121320-d1cfd238.zip', "cluster-runs"))
 # task id: 7d851b4088f1abe608d50ebd78d865ca
