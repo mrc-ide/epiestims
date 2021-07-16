db11717f0ce0b3a799288e59bfb5fb3071530684
sangeetabhatia03 <sangeetabhatia03@gmail.com>
2021-07-16 12:09:30 +0100

    Edits for cluster + use different branch

--- src/one_location_vary_si/vary_si.R
+++ src/one_location_vary_si/vary_si.R
@@ -32,72 +32,19 @@ initial_incidence <- list(
   incidence::incidence(rep(1, 1))
 )
 
-sim_params <- expand.grid(
-  rt_ref = c(1.2, 3),
-  epsilon = c(seq(from = 1, to = 2, by = 0.1), 2.5, 3),
-  si_mu_variant = c(0.5, 0.75, 1, 1.25, 1.5) * si_mu_ref,
-  si_std_variant = si_std_ref
-)
+sim_params <- readRDS("param_grid.rds")
 
 ##############################################################################
 ## Simulate epidemic incidence data with input reproduction numbers and si  ##
 ##############################################################################
-simulated_incid <- pmap(
-  sim_params,
-  function(rt_ref, epsilon, si_mu_variant, si_std_variant) {
-    ## Calculate reproduction number for variant
-    rt_variant <- epsilon * rt_ref
-    ## Assume reproduction number remains the same
-    ## over the time period
-    ## Make a vector that goes across rows
-    R <- array(NA, dim = c(ndays, n_loc, n_v))
-    R[,,1] <- rep(rt_ref, each = ndays)
-    R[,,2] <- rep(rt_variant, each = ndays)
-    si_distr_variant <- discr_si(
-      0:30, mu = si_mu_variant, sigma = si_std_variant
-    )
-    si_distr_variant <- si_distr_variant / sum(si_distr_variant)
-    si_no_zero_var <- si_distr_variant[-1]
-    si_for_sim <- cbind(si_no_zero_ref, si_no_zero_var)
-    ## Because we are starting with a small seed
-    ## we simulate 10 times as many trajectories
-    ## as we need so that we have nsim after
-    ## filtering
-    out <- imap(
-      seq_len(10 * nsims), function(x, index) {
-        message("Simulation ", index)
-        simulate_incidence(
-          initial_incidence, n_loc, n_v, ndays, R, si_for_sim
-        )
-      }
-    )
-    ## total number of cases at the end of the
-    ## simulation for the variant.
-    ncases1 <- map_dbl(out, function(x) sum(x[, , 1]))
-    ncases2 <- map_dbl(out, function(x) sum(x[, , 2]))
-    out <- out[ncases1 > 20 & ncases2 > 20]
-    message("# of simulations with more than 20 cases ", length(out))
-    ## At this point out will have either less than
-    ## or more than the desired number of simulations
-    ## Sample to make sure we have exactly nsim
-    index <- sample(length(out), size = nsims, replace = TRUE)
-    out <- out[index]
-    names(out) <- seq_len(nsims)
-    out
-  }
-)
-
+simulated_incid <- readRDS("sim_incid.rds")
 
 tmax_all <- seq(20, 60, by = 10)
 names(tmax_all) <- tmax_all
 
 ## Estimate epsilon
-results <- pmap(
-  list(
-    incid = simulated_incid,
-    si_mu_variant = sim_params$si_mu_variant,
-    si_std_variant = sim_params$si_std_variant
-  ),
+results <- clusterMap(
+  NULL,
   function(incid, si_mu_variant, si_std_variant) {
     si_distr_variant <- discr_si(
       0:30, mu = si_mu_variant, sigma = si_std_variant
@@ -118,9 +65,11 @@ results <- pmap(
     )
     }
     )
-  }
+  },
+  simulated_incid, sim_params$si_mu_variant, sim_params$si_std_variant
 )
 
-saveRDS(sim_params, "param_grid.rds")
+
+##saveRDS(sim_params, "param_grid.rds")
 saveRDS(results, "estimate_joint_output.rds")
-saveRDS(simulated_incid, "sim_incid.rds")
+##saveRDS(simulated_incid, "sim_incid.rds")
