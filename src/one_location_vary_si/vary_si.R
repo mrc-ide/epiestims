0fe6373f8b14f69c210e119ff450b0e8e16f38bc
sangeetabhatia03 <sangeetabhatia03@gmail.com>
2021-07-21 11:45:56 +0100

    Edits

--- src/one_location_vary_si/vary_si.R
+++ src/one_location_vary_si/vary_si.R
@@ -31,15 +31,14 @@ sim_params <- expand.grid(
   si_mu_variant = c(0.5, 0.75, 1, 1.25, 1.5) * si_mu_ref,
   si_std_variant = si_std_ref
 )
-sim_params <- ifelse(
-  short_run, sim_params[1:2, ], sim_params
-)
+rows <- ifelse(short_run, 2, nrow(sim_params))
+sim_params <- sim_params[seq_len(rows), ]
 incid_init <- initial_incidence()
 ##############################################################################
 ## Simulate epidemic incidence data with input reproduction numbers and si  ##
 ##############################################################################
-simulated_incid <- clusterMap(
-  NULL,
+simulated_incid <- pmap(
+  sim_params,
   function(rt_ref, epsilon, si_mu_variant, si_std_variant) {
     si_distr_variant <- discr_si(
       0:30, mu = si_mu_variant, sigma = si_std_variant
@@ -49,19 +48,20 @@ simulated_incid <- clusterMap(
     si_for_sim <- cbind(si_no_zero_ref, si_no_zero_var)
     simulate_incid_wrapper(
       rt_ref, epsilon, si_for_sim, incid_init = incid_init, nsims = nsims)
-  },
-  sim_params$rt_ref, sim_params$epsilon,
-  sim_params$si_mu_variant,
-  sim_params$si_std_variant
+  }
 )
 
 
-tmax_all <- seq(20, 60, by = 10)
+tmax_all <- seq(10, 50, by = 10)
 names(tmax_all) <- tmax_all
 
 ## Estimate epsilon
-results <- clusterMap(
-  NULL,
+results <- pmap(
+  list(
+    incid = simulated_incid,
+    si_mu_variant = sim_params$si_mu_variant,
+    si_std_variant = sim_params$si_std_variant
+  ),
   function(incid, si_mu_variant, si_std_variant) {
     si_distr_variant <- discr_si(
       0:30, mu = si_mu_variant, sigma = si_std_variant
@@ -73,18 +73,21 @@ results <- clusterMap(
     ## Loop over the first dimension which is
     ## the set of simulations
     map(incid, function(x) {
+      t_min <- EpiEstim::compute_t_min(x, si_for_est)
+      t_max <- as.integer(t_min + tmax)
+      t_max <- min(t_max, nrow(x))
       EpiEstim:::estimate_joint(
         x, si_for_est, priors, seed = 1,
-        t_min = NULL
-        t_max = tmax,
+        t_min = t_min,
+        t_max = t_max,
         mcmc_control = mcmc_controls
       )
+
     }
     )
     }
     )
-  },
-  simulated_incid, sim_params$si_mu_variant, sim_params$si_std_variant
+  }
 )
 
 
