ee54f5da8e22ea68fc41ff161e4b57d25db1b37b
j-wardle <j.wardle19@imperial.ac.uk>
2021-08-17 15:36:34 +0100

    updated with latest file locations for task re-run

--- run_on_cluster.R
+++ run_on_cluster.R
@@ -14,12 +14,12 @@ packages <- c("dplyr", "furrr", "glue", "incidence",
 
 root <- 'context'
 ctx <-context_save(root, packages = packages)
-# [ init:id   ]  b02a8a928368b89edd4c4aecc42a6f58
+# [ init:id   ]  6b39f6628ea252c37ab705e9aa91861c
 # [ init:db   ]  rds
 # [ init:path ]  context
-# [ save:id   ]  542c1033c55c493e03cf574fba298f71
-# [ save:name ]  churnable_nightheron
-# ctx <- context_read('542c1033c55c493e03cf574fba298f71', root)
+# [ save:id   ]  f1d1ce5a48c80487db1dfe49a7e2c8b0
+# [ save:name ]  centrifugal_paintedladybutterfly
+# ctx <- context_read('f1d1ce5a48c80487db1dfe49a7e2c8b0', root)
 
 
 a <- orderly::orderly_run("sims_one_location_step",
@@ -42,7 +42,7 @@ obj$install_packages('mrc-ide/EpiEstim@multiv')
 # orderly_bundle_run arguments
 # 1) where bundle_pack was saved (find with bundle$path)
 # 2) where bundle_run outputs should go (seem to need to specify full file path)
-res <- obj$enqueue(orderly::orderly_bundle_run("Q:\\cluster\\20210813-175522-5ed36934.zip",
+res <- obj$enqueue(orderly::orderly_bundle_run("Q:\\cluster\\20210817-114617-7314fda5.zip",
                                                "Z:\\jwardle\\epiestims\\cluster-runs"))
 # task id: bdb4fe069f5f908f5565af1c5b162079
 
