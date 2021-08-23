0f1b54f4192fa4e0e21e39d6acd3f5e87df3ca08
sangeetabhatia03 <sangeetabhatia03@gmail.com>
2021-08-23 16:13:18 +0100

    Debugging

--- src/sims_one_location_underreporting/sims_one_location_underreport.R
+++ src/sims_one_location_underreporting/sims_one_location_underreport.R
@@ -1,3 +1,4 @@
+## orderly::orderly_develop_start(parameters = list(short_run = TRUE))
 source("R/sim_utils.R")
 set.seed(42)
 
@@ -21,13 +22,19 @@ sim_params <- expand.grid(
   rt_ref = c(1.1, 1.6),
   #epsilon = c(seq(from = 1, to = 2, by = 0.1), 2.5, 3),
   epsilon = 2,
-  p_report = 1
+  p_report = 0.8
 )
 
 # Number of simulations
 nsims <- ifelse(short_run, 1, 100)
 rows <- ifelse(short_run, 2, nrow(sim_params))
 sim_params <- sim_params[seq_len(rows), ]
+si_distr_variant <- discr_si(
+  0:30, mu = si_mu_variant, sigma = si_std_variant
+)
+si_distr_variant <- si_distr_variant / sum(si_distr_variant)
+si_no_zero_var <- si_distr_variant[-1]
+si_for_sim <- cbind(si_no_zero_ref, si_no_zero_var)
 
 ##############################################################################
 ## Simulate epidemic incidence data with input reproduction numbers and si  ##
@@ -36,30 +43,23 @@ plan(multicore)
 simulated_incid <- future_pmap(
   sim_params,
   function(rt_ref, epsilon, p_report) {
-    si_distr_variant <- discr_si(
-      0:30, mu = si_mu_variant, sigma = si_std_variant
-    )
-    si_distr_variant <- si_distr_variant / sum(si_distr_variant)
-    si_no_zero_var <- si_distr_variant[-1]
-    si_for_sim <- cbind(si_no_zero_ref, si_no_zero_var)
     if (rt_ref > 1) incid_init <- initial_incidence("growing")
     else incid_init <- initial_incidence("falling")
-    
     simulate_incid_wrapper(
       rt_ref, epsilon, si_for_sim, incid_init = incid_init, nsims = nsims, p_report = p_report)
   }, .options = furrr_options(seed = TRUE, stdout = FALSE)
 )
-
-si_for_est <- future_pmap(
-  sim_params,
-  function(rt_ref, epsilon, si_mu_variant, si_std_variant) {
-    si_distr_variant <- discr_si(
-      0:30, mu = si_mu_variant, sigma = si_std_variant
-    )
-    si_distr_variant <- si_distr_variant / sum(si_distr_variant)
-    cbind(si_distr_ref, si_distr_variant)
-  }, .options = furrr_options(seed = TRUE)
-)
+si_for_est <- cbind(si_distr_ref, si_distr_variant)
+## si_for_est <- future_pmap(
+##   sim_params,
+##   function(rt_ref, epsilon, si_mu_variant, si_std_variant) {
+##     si_distr_variant <- discr_si(
+##       0:30, mu = si_mu_variant, sigma = si_std_variant
+##     )
+##     si_distr_variant <- si_distr_variant / sum(si_distr_variant)
+##     cbind(si_distr_ref, si_distr_variant)
+##   }, .options = furrr_options(seed = TRUE)
+## )
 
 saveRDS(simulated_incid, "incid.rds")
 saveRDS(si_for_est, "si_for_est.rds")
