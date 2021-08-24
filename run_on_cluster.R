96d1bcb9f30b992c05f825ff4bf45b19af86d16d
rebeccanash <r.nash@imperial.ac.uk>
2021-08-24 11:23:05 +0100

    edit run on cluster file

--- run_on_cluster.R
+++ run_on_cluster.R
@@ -1,16 +1,50 @@
-config <- didehpc::didehpc_config(cores = 8, parallel = TRUE)
+library(orderly)
 library(context)
-packages <- c("dplyr", "epitrix", "glue", "incidence",
-              "orderly", "projections", "purrr")
-root <- 'context'
+
+config <- didehpc::didehpc_config(cores = 8, parallel = TRUE)
+
+packages <- c("dplyr", "epitrix", "furrr", "glue", "incidence",
+              "projections", "purrr")
+
+root <- 'underreport'
 ctx <-context_save(root, packages = packages)
-# [ open:db   ]  rds
-# [ save:id   ]  ed59bb10737dc761b869e66663baef1d
-# [ save:name ]  unadored_lemur
-ctx <- context_read('ed59bb10737dc761b869e66663baef1d', root)
-# orderly::orderly_bundle_pack(".", "one_location_vary_si", parameters = list(short_run = FALSE))
+#[ init:id   ]  4b34ac5ee19b12d136267421b8027cd0
+#[ init:db   ]  rds
+#[ init:path ]  underreport
+#[ save:id   ]  158bf86a28432169c9de38d6a2a2dd48
+#[ save:name ]  batty_hogget
+# Line number 13 is Needed next time you login to the cluster
+# ctx <- context_read('158bf86a28432169c9de38d6a2a2dd48', root)
+
+# pack your task so that it can be run on the cluster:
+orderly::orderly_bundle_pack(".", "sims_one_location_underreporting", 
+                             parameters = list(short_run = FALSE))
+#$id
+#[1] "20210823-165536-ac28064f"
+
+#$path
+#[1] "Z:\\rnash\\epiestims\\20210823-165536-ac28064f.zip"
+
 obj <- didehpc::queue_didehpc(ctx, config = config)
 obj$install_packages('abind')
-obj$install_packages('mrc-ide/EpiEstim@draw_eps_debug')
-res <- obj$enqueue(orderly::orderly_bundle_run('20210716-121320-d1cfd238.zip', "cluster-runs"))
-# task id: 7d851b4088f1abe608d50ebd78d865ca
+obj$install_packages('mrc-ide/EpiEstim@return_diag')
+obj$install_packages("vimc/orderly@fix-zip-list")
+
+# Now run your task on the server
+# 1) first argument is the $path from orderly_bundle_pack
+# 2) second argument is the dir where you want orderly to write outputs
+t1 <- obj$enqueue(orderly::orderly_bundle_run("Z:\\rnash\\epiestims\\20210823-165536-ac28064f.zip", 
+                                              "Z:\\rnash\\epiestims\\cluster-runs"))
+# t1$id b1d05d9ea2473d57a687ac1a56f6a88f
+# check t1$status()
+# when t1$status() is complete, look for t1$result()
+
+#$id
+#[1] "20210823-165536-ac28064f"
+#$path
+#[1] "Z:\\rnash\\epiestims\\cluster-runs\\20210823-165536-ac28064f.zip"
+
+# now run orderly::orderly_bundle_import(<path from t1$result()>)
+orderly::orderly_bundle_import("Z:\\rnash\\epiestims\\cluster-runs\\20210823-165536-ac28064f.zip")
+
+
