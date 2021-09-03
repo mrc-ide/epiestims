8f8a1b4a29d4843c6db944c92264744877d35693
rebeccanash <r.nash@imperial.ac.uk>
2021-09-03 15:34:05 +0100

    reformatted summary script

--- src/summarise_one_location_underreporting/summarise_sims.R
+++ src/summarise_one_location_underreporting/summarise_sims.R
@@ -3,6 +3,7 @@
 prefix <- "underreport"
 source("R/utils.R")
 source("R/summarise_sims.R")
+
 x <- as.list(sim_params)
 x <- append(x, list(summary = eps_summary))
 
@@ -14,103 +15,15 @@ eps_summary_df <- pmap_dfr(
     summary
   }
 )
+saveRDS(eps_summary_df, "eps_summary_df.rds")
 
-eps_summary_df <- mutate_at(
-  eps_summary_df, vars(`2.5%`:`sd`), round, 3
-)
-eps_summary_df$true_eps <- round(eps_summary_df$true_eps, 3)
-
-
-## Summarise by parameters that vary
-## 1. by tmax
-by_tmax <- split(eps_summary_df, eps_summary_df$tmax) %>%
-  map_dfr(
-    function(x) summarise_sims(na.omit(x)), .id = "tmax"
-  )
-## 2. by epsilon
-by_eps <- split(eps_summary_df, eps_summary_df$true_eps) %>%
-  map_dfr(
-    function(x) summarise_sims(na.omit(x)), .id = "true_eps"
-)
-## 3. by p_report
-by_p_report <- split(eps_summary_df, eps_summary_df$p_report) %>%
-  map_dfr(
-    function(x) summarise_sims(na.omit(x)), .id = "p_report"
-  )
-## 4. by rt_ref
-by_rt_ref <- split(eps_summary_df, eps_summary_df$rt_ref) %>%
-  map_dfr(
-    function(x) summarise_sims(na.omit(x)), .id = "rt_ref"
-  )
-## 5. by rt_ref and p_report
-by_rt_and_p_report <- split(
-  eps_summary_df, list(eps_summary_df$rt_ref, eps_summary_df$p_report),
-  sep = "_"
-) %>%
-  map_dfr(
-    function(x) summarise_sims(na.omit(x)), .id = "var"
-  )
-## 6. by all variables
-by_all_vars <-  split(
+## Summarise by all variables
+by_all_vars <-  group_by(
   eps_summary_df,
-  list(eps_summary_df$rt_ref, eps_summary_df$p_report, eps_summary_df$tmax),
-  sep = "_"
-) %>%
-  map_dfr(
-    function(x) summarise_sims(na.omit(x)), .id = "var"
-  )
+  rt_ref, p_report, tmax, true_eps
+) %>% summarise_sims
 
-by_all_vars <- tidyr::separate(by_all_vars, col = "var", into = c("rt_ref", "p_report", "tmax"), sep = "_")
-by_all_vars$p_report <- as.factor(by_all_vars$p_report)
-by_all_vars$rt_ref <- as.factor(by_all_vars$rt_ref)
-
-p <- ggplot(by_all_vars) +
-  geom_point(aes(tmax, pt_est, col = rt_ref)) +
-  geom_linerange(aes(tmax, ymin = lower, ymax = upper, col = rt_ref)) +
-  geom_hline(yintercept = 0.95, linetype = "dashed") +
-  ylab("Proportion in 95% CrI") +
-  xlab("tmax") +
-  ylim(0, 1) +
-  facet_wrap(~ p_report, nrow = 3) +
-  theme_minimal() +
-  labs(color = "Reference Rt") +
-  theme(
-    legend.position = "top"
-  )
-
-ggsave(glue("figures/underreport_prop_in_95.png"), p)
-
-## Proportion in 95% CrI by tmax
-by_all_vars <-  split(
-  eps_summary_df,
-  list(eps_summary_df$rt_ref, eps_summary_df$true_eps, eps_summary_df$p_report, eps_summary_df$tmax),
-  sep = "_"
-) %>%
-  map_dfr(
-    function(x) summarise_sims(na.omit(x)), .id = "var"
-  )
-
-by_all_vars <- tidyr::separate(by_all_vars, col = "var", into = c("rt_ref", "true_eps", "p_report", "tmax"), sep = "_")
-by_all_vars$rt_ref <- as.factor(by_all_vars$rt_ref)
-by_all_vars$p_report <- as.factor(by_all_vars$p_report)
-eps_vals <- unique(by_all_vars$true_eps)
-## To include in manuscript
-by_all_vars$rt_ref <- factor(by_all_vars$rt_ref)
-by_all_vars$true_eps <- factor(by_all_vars$true_eps, levels = eps_vals, ordered = TRUE)
-
-ms_tmax <- "50"
-ms_df <- by_all_vars[by_all_vars$tmax == ms_tmax, ]
-si_df <- by_all_vars[by_all_vars$tmax != ms_tmax, ]
-p1 <- true_epsilon_vs_95CrI(ms_df)+
-  facet_wrap(~p_report)
-ggsave(glue("figures/{prefix}_by_true_eps_tmax_{ms_tmax}.png"), p1)
-
-p2 <- true_epsilon_vs_95CrI(by_all_vars) +
-  facet_wrap(~tmax + p_report)
-
-ggsave(glue("figures/{prefix}_by_true_eps_tmax_report.png"), p2)
-
-
+saveRDS(by_all_vars, "eps_summary_by_all_vars.rds")
 
 ## Error
 x <- as.list(sim_params)
@@ -124,61 +37,13 @@ eps_err_summary_df <- pmap_dfr(
     summary
   }
 )
-eps_err_summary_df <- na.omit(eps_err_summary_df)
 
-x <- group_by(eps_err_summary_df, rt_ref, true_eps, p_report, tmax) %>%
+saveRDS(eps_err_summary_df, "eps_err_summary_df.rds")
+
+x <- group_by(eps_err_summary_df, rt_ref, p_report, true_eps, tmax) %>%
   summarise(
     low = quantile(`50%`, 0.025), med = quantile(`50%`, 0.5),
     high = quantile(`50%`, 0.975)
-  )
-x <- ungroup(x)
-x$rt_ref <- as.factor(x$rt_ref)
-x$p_report <- as.factor(x$p_report)
-x$true_eps <- factor(x$true_eps, levels = eps_vals, ordered = TRUE)
+  ) %>% ungroup()
 
-p1 <- true_epsilon_vs_error(x) +
-  facet_wrap(~p_report)
-
-ggsave(glue("figures/{prefix}_by_true_eps_error.png"), p1)
-
-
-# Facet by rt_ref
-x50 <- filter(x, tmax==50)
-x40 <- filter(x, tmax==40)
-x30 <- filter(x, tmax==30)
-x20 <- filter(x, tmax==20)
-x10 <- filter(x, tmax==10)
-
-plot_underreport <- function(x){
-  p <- ggplot(x) +
-    geom_point(
-      aes(true_eps, med, col = p_report),
-      position = position_dodge(width = 0.3)
-    ) +
-    geom_linerange(
-      aes(true_eps, ymin = low, ymax = high, col = p_report),
-      position = position_dodge(width = 0.3)
-    ) +
-    geom_hline(yintercept = 0, linetype = "dashed") +
-    ylab("Estimated - True transmission advantage") +
-    xlab("True transmission advantage") +
-    theme_minimal() +
-    labs(color = "Probability of being reported") +
-    theme(legend.position = "top")
-  p + facet_wrap(~rt_ref, dir="v", labeller = labeller(rt_ref = rt.label))
-}
-
-rt.label = c("Rt reference 1.1","Rt reference 1.6")
-names(rt.label) = c(1.1,1.6)
-t50 <- plot_underreport(x50)
-t40 <- plot_underreport(x40)
-t30 <- plot_underreport(x30)
-t20 <- plot_underreport(x20)
-t10 <- plot_underreport(x10)
-
-ggsave(glue("figures/{prefix}_by_true_eps_error_tmax50.png"), t50)
-
-saveRDS(eps_err_summary, "eps_err_summary.rds")
-saveRDS(eps_summary_df, "eps_summary_df.rds")
-saveRDS(incid_summary, "incid_summary.rds")
-
+saveRDS(x, "err_summary_by_all_vars.rds")
