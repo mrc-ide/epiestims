1990fe0ebaec6238dec40e90184f8c8d09decefe
j-wardle <j.wardle19@imperial.ac.uk>
2021-09-09 19:05:45 +0100

    cluster script

--- run_on_cluster2lstep.R
+++ run_on_cluster2lstep.R
@@ -14,26 +14,34 @@ packages <- c("dplyr", "furrr", "glue", "incidence",
 
 root <- 'context'
 ctx <-context_save(root, packages = packages)
-# [ init:id   ]  340a337ed2403fbbddb633d2200a2f4b
+# [ init:id   ]  2f08608760e2b524f962997a01ff6e13
 # [ init:db   ]  rds
 # [ init:path ]  context
-# [ save:id   ]  81a4a89d9647e2521c808e4ab78b836a
-# [ save:name ]  useful_jumpingbean
-# ctx <- context_read('81a4a89d9647e2521c808e4ab78b836a', root)
+# [ save:id   ]  4604712b0d5bae09f2a9421e7fb191b4
+# [ save:name ]  unisex_vervet
+# ctx <- context_read('4604712b0d5bae09f2a9421e7fb191b4', root)
 
 
-a <- orderly::orderly_run("sims_one_location_step",
-                          parameters = list(short_run = TRUE))
-b <- orderly::orderly_run("sims_two_location_step",
+a <- orderly::orderly_run("sims_two_location_step",
                           parameters = list(short_run = FALSE))
+b <- orderly::orderly_run("sims_two_location_step_diff",
+                          parameters = list(short_run = FALSE))
+orderly::orderly_commit(a)
 orderly::orderly_commit(b)
 
 path_bundles <- "Q:/cluster"
 
-bundle <- orderly::orderly_bundle_pack(path_bundles, "two_location_step",
+bundle1 <- orderly::orderly_bundle_pack(path_bundles, "two_location_step",
                              parameters = list(short_run = FALSE))
 
-bundle$path # bundle path name to include in orderly_bundle_run
+bundle1$path # bundle path name to include in orderly_bundle_run
+# "Q:\\cluster\\20210820-180319-855b7923.zip"
+
+bundle2 <- orderly::orderly_bundle_pack(path_bundles, "two_location_step_diff",
+                                        parameters = list(short_run = FALSE))
+
+bundle2$path # bundle path name to include in orderly_bundle_run
+# "Q:\\cluster\\20210820-180432-3b94fc45.zip"
 
 obj <- didehpc::queue_didehpc(ctx, config = config)
 obj$install_packages('abind')
@@ -42,8 +50,13 @@ obj$install_packages('mrc-ide/EpiEstim@multiv')
 # orderly_bundle_run arguments
 # 1) where bundle_pack was saved (find with bundle$path)
 # 2) where bundle_run outputs should go (seem to need to specify full file path)
-res <- obj$enqueue(orderly::orderly_bundle_run("Q:\\cluster\\20210820-152308-fe544983.zip",
+res1 <- obj$enqueue(orderly::orderly_bundle_run("Q:\\cluster\\20210820-180319-855b7923.zip",
                                                "Z:\\jwardle\\epiestims\\cluster-runs"))
+
+res2 <- obj$enqueue(orderly::orderly_bundle_run("Q:\\cluster\\20210820-180432-3b94fc45.zip",
+                                                "Z:\\jwardle\\epiestims\\cluster-runs"))
 # task id: bdb4fe069f5f908f5565af1c5b162079
 
-orderly::orderly_bundle_import("Z:\\jwardle\\epiestims\\cluster-runs\\20210813-175522-5ed36934.zip")
+orderly::orderly_bundle_import("Z:\\jwardle\\epiestims\\cluster-runs\\20210820-180319-855b7923.zip")
+
+orderly::orderly_bundle_import("Z:\\jwardle\\epiestims\\cluster-runs\\20210820-180432-3b94fc45.zip")
