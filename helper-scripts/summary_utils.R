98329d5a6fea2d4eb6ec7b9079e4d2083c481e70
rebeccanash <r.nash@imperial.ac.uk>
2022-06-10 10:47:50 +0100

    update helper script

--- helper-scripts/summary_utils.R
+++ helper-scripts/summary_utils.R
@@ -83,7 +83,12 @@ summarise_sims <- function(df) {
     total = n(),
     pt_est = Hmisc::binconf(x = n, n = total, alpha = 0.05)[1, 1],
     lower = Hmisc::binconf(x = n, n = total, alpha = 0.05)[1, 2],
-    upper = Hmisc::binconf(x = n, n = total, alpha = 0.05)[1, 3]
+    upper = Hmisc::binconf(x = n, n = total, alpha = 0.05)[1, 3],
+    n50 = sum(true_eps >= `25%` & true_eps <= `75%`),
+    total50 = n(),
+    pt_est50 = Hmisc::binconf(x = n50, n = total50, alpha = 0.05)[1, 1],
+    lower50 = Hmisc::binconf(x = n50, n = total50, alpha = 0.05)[1, 2],
+    upper50 = Hmisc::binconf(x = n50, n = total50, alpha = 0.05)[1, 3]
   )
 }
 
